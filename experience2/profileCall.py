bc58d9e5120b3f65b32fbf3703595329a16d5c90
AvaPunksmash <ava_punksmash22@protonmail.com>
2019-02-14 21:42:38 -0500

    line profiling and c syscall bench

--- experience2/profileCall.py
+++ experience2/profileCall.py
@@ -1,9 +1,36 @@
-import timeit, random, os, csv
-
+import timeit, os, csv, time, line_profiler
+import random
 nb_repeats = 8
-csv_file = open("output.csv", "w+")
-out_writer = csv.writer(csv_file)
-out_writer.writerow(["hardware_type", "file_system", "function", "iteration", "seek_distance", "time"])#write header
+
+def setup(filename):
+    os.system('sync; echo 3 | sudo tee /proc/sys/vm/drop_caches')
+    file = open(filename, "r")
+    filesize = os.stat(filename).st_size
+    random.seed(0)
+    return file, filesize
+
+@profile
+def seeks_random_profile():
+
+    print("tmpfs")
+
+    for i in range(nb_repeats):
+        file,filesize = setup('/dev/shm/history_of_maths.txt')
+
+        for j in range(10000000):
+            seek_dist = random.randint(0, filesize);
+            file.seek(seek_dist)
+        file.close()
+
+    print("hdd")
+
+    for i in range(nb_repeats):
+        file,filesize = setup('/run/media/user/HDD 1TB/workspace/bigdataneurolab/history_of_maths.txt')
+
+        for j in range(10000000):
+            seek_dist = random.randint(0, filesize);
+            file.seek(seek_dist)
+        file.close()
 
 def seeks_(seek_dist):
     setup = '''import os; \
@@ -17,6 +44,7 @@ def seeks_(seek_dist):
                     number=10000000,
                     repeat=nb_repeats)
 
+    print("tmpfs")
     for i, t in enumerate(times):
         out_writer.writerow(["ram", "tmpfs", "seeks_", str(i), str(seek_dist), str(t)])
 
@@ -29,10 +57,12 @@ def seeks_(seek_dist):
                     stmt=stmt,
                     number=10000000,
                     repeat=nb_repeats)
+
+    print("hdd")
     for i, t in enumerate(times):
         out_writer.writerow(["hdd", "ext4", "seeks_", str(i), str(seek_dist), str(t)])
 
-def random():
+def random_():
     setup = '''import os, random; \
     filename = '/dev/shm/history_of_maths.txt'; \
     filesize = os.stat(filename).st_size; \
@@ -43,6 +73,8 @@ def random():
                     stmt=stmt,
                     number=10000000,
                     repeat=nb_repeats)
+
+    print("random")
     for i, t in enumerate(times):
         out_writer.writerow(["","", "random", str(i), "", str(t)])
 
@@ -60,20 +92,24 @@ def seeks_random():
                     stmt=stmt,
                     number=10000000,
                     repeat=nb_repeats)
+
+    print("tmpfs")
     for i, t in enumerate(times):
         out_writer.writerow(["ram", "tmpfs", "seeks_random", str(i), "", str(t)])
 
-    setup = '''import os, random; \
+    setup2 = '''import os, random; \
     filename = '/run/media/user/HDD 1TB/workspace/bigdataneurolab/history_of_maths.txt'; \
     os.system('sync; echo 3 | sudo tee /proc/sys/vm/drop_caches'); \
     file = open(filename, "r"); \
     filesize = os.stat(filename).st_size; \
     random.seed(0)'''
 
-    times=timeit.repeat(setup=setup,
+    times=timeit.repeat(setup=setup2,
                     stmt=stmt,
                     number=10000000,
                     repeat=nb_repeats)
+
+    print("hdd")
     for i, t in enumerate(times):
         out_writer.writerow(["hdd", "ext4", "seeks_random", str(i), "", str(t)])
 
@@ -95,14 +131,12 @@ def seek_and_(seek_dist, isread):
     file." + function + ";\
     file.seek(0);"
 
-    if isread:
-        stmt = stmt + "os.system('sync; echo 3 | sudo tee /proc/sys/vm/drop_caches');"
-
     times=timeit.repeat(setup=setup,
                     stmt=stmt,
                     number=10000000,
                     repeat=nb_repeats)
 
+    print("tmpfs")
     for i, t in enumerate(times):
         out_writer.writerow(["ram", "tmpfs", "seek_and_" + func_name, str(i), str(seek_dist), str(t)])
 
@@ -116,13 +150,22 @@ def seek_and_(seek_dist, isread):
                     number=10000000,
                     repeat=nb_repeats)
 
+    print("hdd")
     for i, t in enumerate(times):
         out_writer.writerow(["hdd", "ext4", "seek_and_" + func_name, str(i), str(seek_dist), str(t)])
 
 
 if __name__ == "__main__":
+    seeks_random_profile()
+
+    """
+    csv_file = open("output.csv", "w+")
+    out_writer = csv.writer(csv_file)
+    out_writer.writerow(["hardware_type", "file_system", "function", "iteration", "seek_distance", "time"])#write header
+
+
     print("RANDOM")
-    random()
+    random_()
 
     print("RANDOM SEEKS")
     seeks_random()
@@ -141,5 +184,4 @@ if __name__ == "__main__":
     for seek_dist in [0, 100, 1000, 5000, 10000, 50000, 100000, 500000]:
         print('seek dist :', seek_dist)
         seeks_(seek_dist)
-
-    csv_file.close()
+    """
