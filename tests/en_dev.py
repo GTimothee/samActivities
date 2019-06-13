8b98e21f0349272cec04b80b89838d54299d5fe0
GTimothee <ava_punksmash@protonmail.com>
2019-06-12 23:58:13 -0400

    all tests working

--- tests/en_dev.py
+++ tests/en_dev.py
@@ -13,9 +13,15 @@ def add_or_create_to_list_dict(d, k, v):
 def get_keys_from_graph(graph, printer=False):
     key_dict = dict()
     for k, v in graph.items():
-        split = k.split('-')
-        key_name = split[:len(split)]
-        add_or_create_to_list_dict(key_dict, key_name, k)
+        if isinstance(k, tuple):
+            k2 = k[0]
+        elif isinstance(k, str):
+            k2 = k 
+        else:
+            raise ValueError("type of key unsupported", k, type(k))
+        split = k2.split('-')
+        key_name = "-".join(split[:-1])
+        key_dict = add_or_create_to_list_dict(key_dict, key_name, k)
     return key_dict
 
 
@@ -27,40 +33,45 @@ def get_rechunk_subkeys(rechunk_graph):
 def test_source_key(slices_dict, source_key):
     """ test if source is an array proxy: if yes, add source key data to slices_dict
     """
-        if len(source_key) != 4:
-            raise ValueError("not enough elements to unpack in", source_key)
-        source, s1, s2, s3 = source_key
-        if not isinstance(source, str):
-            raise ValueError("expected a string:", source)
-        if 'array' in source:
-            add_or_create_to_list_dict(slices_dict, source, (s1, s2, s3))
-        return slices_dict
+    if len(source_key) != 4:
+        raise ValueError("not enough elements to unpack in", source_key)
+    if not isinstance(source_key, tuple):
+        raise ValueError("expected a tuple:", source_key)
+    source, s1, s2, s3 = source_key
+    
+    if not isinstance(source, str):
+        raise ValueError("expected a string:", source)
+    if 'array' in source:
+        slices_dict = add_or_create_to_list_dict(slices_dict, source, (s1, s2, s3))
+    return slices_dict
 
 
 def get_slices_from_rechunk_subkeys(rechunk_merge_graph, split_keys, merge_keys):
 
-    def get_slices_from_splits(split_keys):
+    def get_slices_from_splits(split_keys, slices_dict):
         for split_key in split_keys:
+            
             split_value = rechunk_merge_graph[split_key]
             _, source_key, slices = split_value
             slices_dict = test_source_key(slices_dict, source_key)
         return slices_dict
 
     # TODO: make better
-    def get_slices_from_merges(merge_keys):
+    def get_slices_from_merges(merge_keys, slices_dict):
         for merge_key in merge_keys:
             merge_value = rechunk_merge_graph[merge_key]
             _, concat_list = merge_value
-            while not isinstance(l[0][0], tuple):
-                l = l[0]
-            for block in l:
+            while not isinstance(concat_list[0][0], tuple):
+                concat_list = concat_list[0]
+            for block in concat_list:
                 for source_key in block:
-                    slices_dict = test_source_key(slices_dict, source_key)
+                    if len(source_key) == 4:
+                        slices_dict = test_source_key(slices_dict, source_key)
         return slices_dict
 
     slices_dict = dict()
-    slices_dict = get_slices_from_splits(rechunk_splits, slices_dict)
-    slices_dict = get_slices_from_merges(rechunk_merges, slices_dict)
+    slices_dict = get_slices_from_splits(split_keys, slices_dict)
+    slices_dict = get_slices_from_merges(merge_keys, slices_dict)
     return slices_dict
 
 
@@ -76,10 +87,9 @@ def get_slices_from_rechunk_keys(graph, rechunk_keys):
 
 def get_slices_from_getitem_subkeys(getitem_graph):
     slices_dict = dict()
-    for k, v in getitem_graph:
-        f, a, s = v 
-        source, s1, s2, s3 = a
-        test_source_key(slices_dict, source_key)
+    for k, v in getitem_graph.items():
+        f, source_key, s = v 
+        slices_dict = test_source_key(slices_dict, source_key)
     return slices_dict
 
 
--- tests/tests_funcs.py
+++ tests/tests_funcs.py
@@ -16,14 +16,15 @@ from en_dev import *
 
 def test_add_or_create_to_list_dict():
     d = {'a': [1], 'c': [5, 6]}
-    d = add_or_create_to_list_dict(d, 'b', [2, 3])
-    d = add_or_create_to_list_dict(d, 'b', [5])
+    d = add_or_create_to_list_dict(d, 'b', 2)
+    d = add_or_create_to_list_dict(d, 'b', 3)
+    d = add_or_create_to_list_dict(d, 'b', 4)
     expected = {'a': [1], 'b': [2, 3, 4], 'c': [5, 6]}
     
-    # test 
     if expected != d:
         print("error")
         print(d, "VS", expected)
+        return
     print("it works")
 
 
@@ -33,41 +34,44 @@ def test_get_keys_from_graph():
          'a-864531': 3,
          'c-864535': 4}
     keys_dict = get_keys_from_graph(d, printer=False)
-    expected = {'a': ['b-865134'],
-                'b': ['a-165152', 'a-864531'],
+    expected = {'a': ['a-165152', 'a-864531'],
+                'b': ['b-865134'],
                 'c': ['c-864535']}
     
-    # test 
-    for k, v in expected:
-        if not k in list(keys_dict.keys()):
-            print("error")
-            print(k, "VS", list(keys_dict.keys()))
-            return 
-        if set(keys_dict[k]) != set(v):
-            print("error")
-            print(keys_dict[k], "VS", v)
-            return 
+    if not expected == keys_dict:
+        print("error")
+        print(expected, "VS", keys_dict)
+        return 
     print("it works")
 
 
 def test_get_rechunk_subkeys():
-    d = example_rechunk_dict
+    d = get_rechunk_dict_without_proxy_array_sample()
     split_keys, merge_keys = get_rechunk_subkeys(d)
 
-    expected_split_keys = [('rechunk-merge-bcfb966a39aa5079f6457f1530dd85df', 0, 0, 0),
+    expected_merge_keys = [('rechunk-merge-bcfb966a39aa5079f6457f1530dd85df', 0, 0, 0),
                            ('rechunk-merge-bcfb966a39aa5079f6457f1530dd85df', 0, 0, 1)]
 
-    expected_merge_keys = [('rechunk-split-bcfb966a39aa5079f6457f1530dd85df', 1)
-                           ('rechunk-split-bcfb966a39aa5079f6457f1530dd85df', 2)
-                           ('rechunk-split-bcfb966a39aa5079f6457f1530dd85df', 3)]
+    expected_split_keys = [('rechunk-split-bcfb966a39aa5079f6457f1530dd85df', 0),
+                           ('rechunk-split-bcfb966a39aa5079f6457f1530dd85df', 1),
+                           ('rechunk-split-bcfb966a39aa5079f6457f1530dd85df', 2),
+                           ('rechunk-split-bcfb966a39aa5079f6457f1530dd85df', 3),
+                           ('rechunk-split-bcfb966a39aa5079f6457f1530dd85df', 4),
+                           ('rechunk-split-bcfb966a39aa5079f6457f1530dd85df', 5)]
 
-    if not split_keys == expected_split_keys:
-        print("error")
-        print(split_keys, "VS", expected_merge_keys)
-
-    if not merge_keys == expected_split_keys:
-        print("error")
-        print(merge_keys, "VS", expected_merge_keys)
+    for i, j in zip(split_keys, expected_split_keys):
+        if i != j:
+            print("error")
+            print(i)
+            print(j, '\n')
+            return
+                
+    for i, j in zip(merge_keys, expected_merge_keys):
+        if i != j:
+            print("error")
+            print(i)
+            print(j, '\n')
+            return
     print("it works")
 
 
@@ -77,35 +81,97 @@ def test_test_source_key():
                          slice(None, None, None), 
                          slice(None, None, None), 
                          slice(None, None, None)]
-    expected = {'array-645318645': tuple(sample_source_key)}
+    has_failed = [False, False]
+    expected = {'array-645318645': [(sample_source_key[1], sample_source_key[2], sample_source_key[3])]}
     slices_dict = test_source_key(slices_dict, tuple(sample_source_key))
     sample_source_key[0] = "tmp"
-    slices_dict = test_source_key(slices_dict, tuple(sample_source_key))
-    sample_source_key[0] = 864513
-    slices_dict = test_source_key(slices_dict, tuple(sample_source_key))
+    try:
+        result = test_source_key(slices_dict, tuple(sample_source_key))
+    except:
+        has_failed[0] = True
+    sample_source_key[0] = (864513, 86513, 4651, 3465)
+    try:
+        result = test_source_key(slices_dict, tuple(sample_source_key))
+    except:
+        has_failed[1] = True
     
-    if slices_dict != expected:
-        print("error")
-        print(slices_dict, "VS", expected)
+    if has_failed != [False, True]:
+        print("error in failure management")
+        return
+
+    for i, j in zip(expected, slices_dict):
+        if i != j:
+            print("error")
+            print(i)
+            print(j, '\n')
+            return
     print("it works")
 
     
 def test_get_slices_from_rechunk_subkeys():
-    pass 
+    rechunk_merge_graph = get_rechunk_dict_from_proxy_array_sample()
+    split_keys, merge_keys = get_rechunk_subkeys(rechunk_merge_graph)
+    slices_dict = get_slices_from_rechunk_subkeys(rechunk_merge_graph, split_keys, merge_keys)
+    expected_name = 'array-3ec4eddf5e385f67eb8007734372b503'
+    expected_list = [(0,0,1),(0,0,2),(0,1,0),(0,1,1),(0,1,2),(0,0,3),(0,1,3),(0,2,0),(0,2,1),(0,2,2)]
+    expected = {expected_name: expected_list}
+    if not set(slices_dict) == set(expected):
+        print("error")
+        print(slices_dict)
+        print(expected)
+        return 
+    print("it works")
 
 
 def test_get_slices_from_rechunk_keys():
-    pass 
+    names = ['array-864513645',
+             'array-8645346531',
+             'array-6845312645']
+
+    d = {'rechunk-merge-65312653120': get_rechunk_dict_from_proxy_array_sample(array_names=names, add_list=[1]),
+         'rechunk-merge-64531264531': get_rechunk_dict_from_proxy_array_sample(array_names=names, add_list=[2]),
+         'rechunk-merge-86453126543': get_rechunk_dict_from_proxy_array_sample(array_names=names, add_list=[3])}
+
+    rechunk_keys = list(d.keys())
+
+    slices_dict = get_slices_from_rechunk_keys(d, rechunk_keys)
+    expected = dict()
+    expected[names[0]] = [(0,0,3),(0,1,3)]
+    expected[names[1]] = [(0,2,0)]
+    expected[names[2]] = [(0,2,1),(0,2,2)]
+    expected['array-3ec4eddf5e385f67eb8007734372b503'] = [(0,0,0),(0,0,1),(0,0,2),(0,1,0),(0,1,1),(0,1,2)]
+    
+    for k in list(expected.keys()):
+        if set(expected[k]) != set(slices_dict[k]):
+            print("error at", k)
+            print(slices_dict)
+            print(expected)
+            return 
+    print("it works")
 
 
 def test_get_slices_from_getitem_subkeys():
-    pass
+    getitem_graph = get_getitem_dict_from_proxy_array_sample()
+    slices_dict = get_slices_from_getitem_subkeys(getitem_graph)
+    expected_name = 'array-6f870a321e8529128cb9bb82b8573db5'
+    expected = {expected_name: [(0,0,0),(0,0,1),(0,0,2),(0,0,3),(0,0,4),(0,0,5)]}
+    if not slices_dict == expected:
+        print("error")
+        print(slices_dict)
+        print(expected)
+        return 
+    print("it works")
 
 
 def test_get_slices_from_getitem_keys():
-    pass 
-
-
-def test_get_slices_from_dask_graph():
-    graph = get_graph_for_tests(0)
-    slices_dict = get_slices_from_dask_graph(graph)
+    graph = get_graph_with_getitem()
+    getitem_keys = ['getitem-430f856c4196ad50518e167d79ffd894']
+    slices_dict = get_slices_from_getitem_keys(graph, getitem_keys)
+    expected_name = 'array-4d8aa96f6f06806aeb9a11b75751b175'
+    expected = {expected_name: [(0,0,0),(0,0,1),(0,0,2),(0,0,3),(0,0,4),(0,0,5),(0,0,6)]}
+    if not slices_dict == expected:
+        print("error")
+        print(slices_dict)
+        print(expected)
+        return 
+    print("it works")
